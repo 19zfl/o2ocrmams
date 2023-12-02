36e6b7a7ae39340934e60eb9f2b4dd163f7b666b
19zfl <19aleiya@gmail.com>
2023-12-02 21:06:39 +0800

    interface test

---
+++ src/test/java/com/zfl19/system/mapper/DepartmentMapperTest.java
@@ -0,0 +1,28 @@
+package com.zfl19.system.mapper;
+
+import com.zfl19.App;
+import com.zfl19.system.domain.Department;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class DepartmentMapperTest {
+
+    @Autowired
+    private DepartmentMapper mapper;
+
+    @Test
+    public void test01() {
+        List<Department> departments = mapper.selectAll();
+        departments.forEach(System.err::println);
+    }
+
+}
