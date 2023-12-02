82554f42eeeac0067b825ca9285c904aa0215730
19zfl <19aleiya@gmail.com>
2023-12-02 21:10:49 +0800

    add department domain

---
+++ src/main/java/com/zfl19/system/domain/Department.java
@@ -0,0 +1,51 @@
+package com.zfl19.system.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author: 19zfl
+ * @description: 部门实体类
+ * @date 2023-12-02
+ */
+@Data
+@AllArgsConstructor // 有参构造器
+@NoArgsConstructor  // 无参构造器
+@Table(name = "t_department")   // 当前操作的表
+public class Department {
+
+    /**
+     * 部门id
+     */
+    @Id
+    private Long id;
+    /**
+     * 部门编号
+     */
+    private String sn;
+    /**
+     * 部门名称
+     */
+    private String name;
+    /**
+     * 部门状态
+     */
+    private Integer state;
+    /**
+     * 部门经理id
+     */
+    private Long managerId;
+    /**
+     * 上级部门id
+     */
+    private Long parentId;
+    /**
+     * 部门路径
+     */
+    private String dirPath;
+
+}
