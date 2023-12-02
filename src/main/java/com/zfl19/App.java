f4d0fefc047ca01214f2affe73da4cb1a6511314
19zfl <19aleiya@gmail.com>
2023-12-02 21:07:00 +0800

    add mapper scan by tk

--- src/main/java/com/zfl19/App.java
+++ src/main/java/com/zfl19/App.java
@@ -2,6 +2,7 @@ package com.zfl19;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author: 19zfl
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@MapperScan("com.zfl19.*.mapper")   // 使用通配符，让启动类能够扫描到每个包中的mapper
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
