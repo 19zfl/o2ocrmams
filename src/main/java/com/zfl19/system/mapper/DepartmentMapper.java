9d340565e77baf7038399ccbe6d4bfdad6e926a8
19zfl <19aleiya@gmail.com>
2023-12-02 21:10:59 +0800

    add department mapper

---
+++ src/main/java/com/zfl19/system/mapper/DepartmentMapper.java
@@ -0,0 +1,13 @@
+package com.zfl19.system.mapper;
+
+import com.zfl19.system.domain.Department;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author: 19zfl
+ * @description: 部门持久层
+ * @date 2023-12-02
+ */
+public interface DepartmentMapper extends Mapper<Department> {
+
+}
