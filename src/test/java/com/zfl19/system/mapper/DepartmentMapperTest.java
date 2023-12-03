823fbcc780eb29402062db5d5cd66676cdfae6bc
19zfl <19aleiya@gmail.com>
2023-12-03 17:40:49 +0800

    test redis connection

--- src/test/java/com/zfl19/system/mapper/DepartmentMapperTest.java
+++ src/test/java/com/zfl19/system/mapper/DepartmentMapperTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -19,10 +20,18 @@ public class DepartmentMapperTest {
     @Autowired
     private DepartmentMapper mapper;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Test
     public void test01() {
         List<Department> departments = mapper.selectAll();
         departments.forEach(System.err::println);
+    }
+
+    @Test
+    public void test02() {
+        redisTemplate.opsForValue().set("name", "刘十三");
     }
 
 }
